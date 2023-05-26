4852c3ac751702a56c3647f322c49f9ef06021dc
SigOctantisTheFallen <109712363+SigOctantisTheFallen@users.noreply.github.com>
2023-05-26 17:30:10 +0530

    Update contact.php

--- assets/contact.php
+++ assets/contact.php
@@ -1,11 +1,13 @@
 <?php
-$email_address = $_POST['email_address'];
-$feedback = $_POST['feedback'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
+
 function filter_email_header($form_field) {  
 return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
 }
 $email_address  = filter_email_header($email_address);
 
-$headers = "From: $email_addressn";
-$sent = mail('you@domain.com', 'Feedback Form Submission', $feedback, $headers);
+$headers = "From: $email_address";
+$sent = mail('dhruvgupta@iisc.ac.in', $subject, $feedback, $headers);
 ?>
