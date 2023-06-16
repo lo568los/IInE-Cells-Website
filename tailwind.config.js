1f53bd910ac17a0e6a6453ea32e8fd90d28e8833
lo568los <soumyadeepsarma3@gmail.com>
2023-06-16 12:25:47 +0530

    pages added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class",
   purge: {
     enabled: true,
     content: [
